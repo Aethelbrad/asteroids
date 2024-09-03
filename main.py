77bc91482d6a7b1d416d2648758b2bf4f46a253c
Aethelbrad <brad.mathis@protonmail.com>
2024-09-02 22:26:09 -0700

    last push before submit

--- main.py
+++ main.py
@@ -1,19 +1,59 @@
 import pygame
+
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroid_field import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
+
+    # instantiate the player after adding player to groups
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
+    # instantiate the asteroid field after adding asteroid field to groups
+    asteroidField = AsteroidField()
+
+
     # Main game loop
     while True:
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+        screen.fill(color=(0, 0, 0))
+        for sprite in updatable:
+            sprite.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+        for sprite in asteroids:
+            if sprite.check_collision(player):
+                raise SystemExit("Game Over!")
+        for sprite in asteroids:
+            for shot in shots:
+                if shot.check_collision(sprite):
+                    sprite.split()
+                    shot.kill()
+                    
         pygame.display.flip()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        
+
+        print("Starting asteroids!")
+        print(f"Screen width: {SCREEN_WIDTH}")
+        print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
